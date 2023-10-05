1976d5145055b6f697f785d5a2b32f03186a7aed
Brandon Boatman <bboatman8@gmail.com>
2023-10-05 14:05:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
