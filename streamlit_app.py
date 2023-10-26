512754c3be1484826d1cde4ab0606ab91863327a
Brandon Boatman <bboatman8@gmail.com>
2023-10-26 10:30:45 -0500

    Update streamlit_app.py
    Removed json line, added user input.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,7 +21,8 @@ streamlit.dataframe(fruits_to_show)
 
 #streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.write('The user entered,' fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
